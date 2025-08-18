2f0ba9d4be9bf447ac89fed3a37417b8652f1e8f
Maria Eduarda Campanelli da Rocha Cabo <maria.cabo@aditec.local>
2025-08-18 11:33:13 -0300

    Removido contato e sobre

--- receitas/views.py
+++ receitas/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'receitas/home.html')
-
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
+    return render(request, 'receitas/home.html', context={
+        'name': 'Maria eduarda',
+    })
