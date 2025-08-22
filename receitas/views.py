526b60ea6fa5fb241db81331ff85d9f052108c56
Maria Eduarda Campanelli da Rocha Cabo <maria.cabo@aditec.local>
2025-08-22 11:19:19 -0300

    Criando a pagina da receita e fazendo herança de templates

--- receitas/views.py
+++ receitas/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'receitas/pages/home.html',)
+
+def receita(request, id):
+    return render(request, 'receitas/pages/receita-view.html',)
