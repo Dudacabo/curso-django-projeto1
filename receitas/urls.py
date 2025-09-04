7b9a23a3a624ac16004b496cf83eceeaee07e884
Maria Eduarda Campanelli da Rocha Cabo <maria.cabo@aditec.local>
2025-09-04 09:52:32 -0300

    Ocultando receitas que não estão publicadas

--- receitas/urls.py
+++ receitas/urls.py
@@ -6,5 +6,8 @@ app_name = 'receitas'
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('receitas/category/<int:category_id>/', views.category, name= "category"),
     path('receitas/<int:id>/', views.receita, name= "receita"),
+    
+    
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from utils.receitas.factory import make_receita
+from .models import Receita
+
 
 def home(request):
+    receitas = Receita.objects.filter(is_published = True,).order_by('-id')
     return render(request, 'receitas/pages/home.html', context={
-        'receitas': [make_receita() for _ in range(10)],
+        'receitas': receitas,
+    })
+
+def category(request, category_id):
+    receitas = Receita.objects.filter(
+        category__id=category_id, is_published = True, ).order_by('-id')
+    
+    return render(request, 'receitas/pages/category.html', context={
+        'receitas': receitas,
     })
 
 def receita(request, id):
