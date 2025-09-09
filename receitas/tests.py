7ce1d2f0b4f4ac6982fe313991dfa383adb1ffe1
Maria Eduarda Campanelli da Rocha Cabo <maria.cabo@aditec.local>
2025-09-09 11:59:44 -0300

    Testando urls

--- receitas/tests.py
+++ receitas/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class ReceitaURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1, 'Um é igual a um'
+    def test_receita_home_url_is_correct(self):
+        url = reverse('receitas:home')
+        self.assertEqual(url, '/')
+
+    def test_receita_category_url_is_correct(self):
+        url = reverse('receitas:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/receitas/category/1/')
+
+    def test_receita_detail_url_is_correct(self):
+        url = reverse('receitas:receita', kwargs={'id': 1})
+        self.assertEqual(url, '/receitas/1/')
     
+
