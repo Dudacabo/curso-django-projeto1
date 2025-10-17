53530cd58a54957e8bc8abe2b8c8fc79b6784867
maria.cabo <maria.cabo@itec.al.gov.br>
2025-10-17 10:50:43 -0300

    Testando a string representation da receita

--- receitas/tests/test_receita_models.py
+++ receitas/tests/test_receita_models.py
@@ -46,4 +46,12 @@ class ReceitaModelTest(ReceitaTestBase):
         receita = self.make_receita_no_defaults()            
         self.assertFalse(receita.is_published, msg='Receita is_published não é falso',)
 
+    def test_receita_string_representation(self):
+        needed = 'Testing Representation'
+        self.receita.title = needed
+        self.receita.full_clean()
+        self.receita.save()
+        self.assertEqual(str(self.receita), needed, msg=f'A representação da string de receitas deve ser'f'"{needed}" mas "{str(self.receita)}" que foi recebio.')
+
+
 
