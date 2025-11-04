e1da45c484af137125fa78de22f6d6032276976a
maria.cabo <maria.cabo@itec.al.gov.br>
2025-11-04 10:21:23 -0300

    Configurando as flash messages do django

--- receitas/views.py
+++ receitas/views.py
@@ -4,6 +4,7 @@ from django.http.response import Http404
 from django.db.models import Q
 from utils.pagination import make_pagination
 import os
+from django.contrib import messages
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
@@ -11,6 +12,8 @@ def home(request):
     receitas = Receita.objects.filter(is_published = True,).order_by('-id')
 
     page_obj, pagination_range = make_pagination(request, receitas, PER_PAGE)
+
+    messages.success(request, "Uma mensagem")
 
     return render(request, 'receitas/pages/home.html', context={
         'receitas': page_obj,
